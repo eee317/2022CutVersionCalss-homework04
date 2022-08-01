3915f955a13a4056758f6c9ec482d9939a362cdd
Pei <gonsux.3686@gmail.com>
2022-08-01 21:21:50 +0800

    撰寫 jquery 完成 nav 手機的動態

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,19 @@
 console.log('Hello!');
 
-$(document).ready(() => {
+// $(document).ready(() => {
+//   $('.icon-menu').click(function(e){
+//     $('.mobile-nav-list').toggleClass('.active')
+//   })
+// });
+$(document).ready(function(){
   $('.icon-menu').click(function(e){
-    $('.mobile-nav-list').toggleClass('.active')
+    $('.mobile-nav-list').addClass('active');
+    $('.icon-menu').addClass('none');
+    $('.navbar').addClass('jcc');
   })
-});
+  $('.mobile-nav-list li, .nav-img').click(function(e){
+    $('.navbar').removeClass('jcc');
+    $('.mobile-nav-list').removeClass('active');
+    $('.icon-menu').removeClass('none');
+  })
+})
