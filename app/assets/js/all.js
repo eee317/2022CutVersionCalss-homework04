8d0eeffb821499fe766ca8705d4d1d4bcbae0baf
Pei <gonsux.3686@gmail.com>
2022-08-01 18:14:20 +0800

    Update 2022-08-01T10:14:17.367Z

--- app/assets/js/all.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  $('.icon-menu').click(function(e){
-    $('.mobile-nav-list').toggleClass('.active')
-  })
-});
---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  $('.icon-menu').click(function (e) {
+    $('.mobile-nav-list').toggleClass('.active');
+  });
+});
+//# sourceMappingURL=all.js.map
