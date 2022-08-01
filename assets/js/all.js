e344c04d8265cce60c24776f62b283031fac1fe0
Pei <gonsux.3686@gmail.com>
2022-08-01 23:12:04 +0800

    Update 2022-08-01T15:12:03.011Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,21 @@
 "use strict";
 
-console.log('Hello!');
+console.log('Hello!'); // $(document).ready(() => {
+//   $('.icon-menu').click(function(e){
+//     $('.mobile-nav-list').toggleClass('.active')
+//   })
+// });
+
 $(document).ready(function () {
   $('.icon-menu').click(function (e) {
-    $('.mobile-nav-list').toggleClass('.active');
+    $('.mobile-nav-list').addClass('active');
+    $('.icon-menu').addClass('none');
+    $('.navbar').addClass('jcc');
+  });
+  $('.mobile-nav-list li, .nav-img').click(function (e) {
+    $('.navbar').removeClass('jcc');
+    $('.mobile-nav-list').removeClass('active');
+    $('.icon-menu').removeClass('none');
   });
 });
 //# sourceMappingURL=all.js.map
